8c6183a4fb9996a0215f6525797c6572bca667d5
mattc41190 <mattc41190@gmail.com>
2022-03-18 16:59:20 -0500

    feat: ready for release

--- cmd/reverse/main.go
+++ cmd/reverse/main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"matthewcale.com/reverse/lib"
 )
 
-const (
-	test = "test"
-)
-
 func main() {
-	reversed, err := lib.Reverse(test)
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("reverse requires at least 1 argument")
+		os.Exit(1)
+	}
+
+	arg := strings.Join(args, " ")
+
+	reversed, err := lib.Reverse(arg)
 	if err != nil {
 		fmt.Println("An error occurred exiting")
 		os.Exit(1)
---
+++ dev/build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Assumes running from root of repo
+go build -o bin/reverse cmd/reverse/main.go
